822588ec21a4272e517305ba7cf9c987034fc5e0
Karl Stoney <karl.stoney@hp.com>
2014-02-11 15:28:58 +0000

    set

--- gitflow-installer.sh
+++ gitflow-installer.sh
@@ -69,10 +69,12 @@ case "$1" in
 		install -v -d -m 0755 "$INSTALL_PREFIX"
 		for exec_file in $EXEC_FILES ; do
 			install -v -m 0755 "$REPO_NAME/$exec_file" "$INSTALL_PREFIX"
+			puts "TEST1"
 			dos2unix "$REPO_NAME/$exec_file"
 		done
 		for script_file in $SCRIPT_FILES ; do
 			install -v -m 0644 "$REPO_NAME/$script_file" "$INSTALL_PREFIX"
+			puts "TEST2"
 			dos2unix "$REPO_NAME/$script_file"
 		done
 		exit
